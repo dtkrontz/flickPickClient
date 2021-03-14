bf4286b3fb6a2f5331e98159d7e833a6b3db2cd0
Dtkrontz <dtkrontz@gmail.com>
2021-03-14 15:41:21 -0400

    moving to dustin branch

---
+++ src/components/profile/watchlistDisplay/WatchlistDelete.js
@@ -0,0 +1,19 @@
+
+
+const DeleteWatchlistItem = (props) => {
+    
+    const deleteWatchlist = (watchlist) => {
+        fetch(`http://localhost:3000/watchlist/${watchlist}`, {
+            method: 'DELETE',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': props.token
+            })
+        })
+        .then(() => props.fetchWatchlist())
+    }
+
+    return(
+
+    )
+}
