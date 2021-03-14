ecc228dd56e636e4a08de1d18cde9b0563068fd1
GPMcKelvey <gpmckelvey@gmail.com>
2021-03-14 15:15:45 -0400

    added search, results, and modal

---
+++ src/components/Search.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import {Button} from 'reactstrap';
+import DisplayResult from './DisplayResult';
+
+const SearchComponent = (props) => {
+
+    const [result, setResult] = useState([]);
+    const [display, setDisplay] = useState(false);
+    const [title, setTitle] = useState('');
+
+    const key = '&apikey=87a5adba';
+    const url ='http://www.omdbapi.com/?';
+
+    const searchFetch = async () => { 
+        await fetch(`${url}t=${title}${key}`)
+        .then(res => res.json())
+        .then(json => {
+            setResult([json]);
+            setDisplay(true);
+            //console.log(json);
+        });
+    };
+
+    const handleSubmit = (event) => {
+        setResult([]);
+        searchFetch();
+    }
+
+    useEffect (() => {
+        setDisplay(false);
+    }, []);
+
+    console.log(result);
+
+    return (
+        <div className='home'>
+            <input type="text" placeholder="movie title" onChange={(e) => setTitle(e.target.value)} />
+            <Button onClick={(event) => handleSubmit(event)}>Search</Button>
+            {display ? <DisplayResult result= {result} handleSubmit={handleSubmit}/> : null}
+        </div>
+    )
+};
+
+export default SearchComponent;
