fdf131429819008a706a30077e1bf9f792d61aa1
Jasmine Rarugal <JasmineRarugal@gmail.com>
2021-03-15 17:01:06 -0400

    started the signup form

--- src/components/home/signup/Signup.js
+++ src/components/home/signup/Signup.js
@@ -1,1 +1,25 @@
 // modal to signup and be directed to the profile
+import React, {useState} from 'react';
+import {Modal, ModalHeader, Form, Label} from 'reactstrap';
+ 
+
+const Signup= () =>{
+
+
+
+
+
+
+
+return(
+    <div>
+        <Modal isOpen={Modal} name='signup'>
+            <p>test</p>
+
+        </Modal>
+    </div>
+    
+)
+}
+
+export default Signup;
