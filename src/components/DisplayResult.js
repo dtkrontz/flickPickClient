ac9bee03db4436fcd972c6a930ff76e4808bec87
GPMcKelvey <gpmckelvey@gmail.com>
2021-03-18 17:39:43 -0400

    minor modal changes

--- src/components/DisplayResult.js
+++ src/components/DisplayResult.js
@@ -33,10 +33,10 @@ const DisplayResult = (props) => {
                         <Col>
                             <ListGroup>
                                 <ListGroupItemHeading>{r.Title}</ListGroupItemHeading>
-                                <ListGroupItem>{r.Rated}</ListGroupItem>
-                                <ListGroupItem>{r.Runtime}</ListGroupItem>
-                                <ListGroupItem>{r.Genre}</ListGroupItem>
-                                <ListGroupItem>{r.Plot}</ListGroupItem>
+                                <ListGroupItem color="warning">{r.Rated}</ListGroupItem>
+                                <ListGroupItem color="warning">{r.Runtime}</ListGroupItem>
+                                <ListGroupItem color="warning">{r.Genre}</ListGroupItem>
+                                <ListGroupItem color="warning">{r.Plot}</ListGroupItem>
                             </ListGroup>
                         </Col>
                     </Row>
